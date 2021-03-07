8fbe8f88a1b2dba9a056d0d1bc4d6bb724042f5c
guiyomj <wastfg6972@naver.com>
2021-03-07 22:13:01 +0900

    집가자!

--- src/Intro.js
+++ src/Intro.js
@@ -6,6 +6,9 @@ const Intro = () => {
         <div>
             <div id="intro" className="intro">
                 <img src={title} className="title-img" alt="제목 이미지" />
+                {
+                    /* <Link className="btn start" to="/main">시작하기</Link> */
+                }
                 <img src={start} className="start-btn" alt="시작 버튼" onClick={
                     () => {
                         document.getElementById('intro').classList.add("disappear")
