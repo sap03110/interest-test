ab15b444c911dcf6a509b322a857d8afdb640600
김미정 <mijeonkim@gmarket.com>
2022-10-10 22:52:46 +0900

    feature: Intro 페이지에서 불필요한 클릭 이벤트 코드 삭제

--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -4,15 +4,11 @@ import Title from '../images/title-img.png';
 import Start from '../images/start-btn.png';
 
 const Intro = () => {
-  const handleStart = () => {
-    //document.getElementById('intro').classList.add('disappear');
-  };
-
   return (
     <div id="intro" className="intro">
       <img src={Title} className="title-img" alt="제목 이미지" />
       <Link className="btn" to="/main">
-        <img src={Start} className="start-btn" alt="시작 버튼" onClick={handleStart} />
+        <img src={Start} className="start-btn" alt="시작 버튼" />
       </Link>
     </div>
   );
