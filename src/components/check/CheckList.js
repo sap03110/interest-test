3024fea8704952f9cf96237b288e8740b24a98ff
김미정 <mijeonkim@gmarket.com>
2022-10-11 16:47:07 +0900

    refactor: 체크박스 컴포넌트 분리 && 체크리스트 로직 변경

---
+++ src/components/check/CheckList.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from 'react';
+
+import useStore from '../../store';
+import CheckBox from './CheckBox';
+
+const CheckList = ({ selections }) => {
+  const [checksInfo, setChecksInfo] = useState(selections.map((content) => ({ content, checked: false })));
+  const checkAnswerSelected = useStore((state) => state.checkAnswerSelected);
+
+  const toggleCheck = (content) => {
+    setChecksInfo((prevChecksInfo) =>
+      prevChecksInfo.map((checkInfo) => {
+        if (content === checkInfo.content) return { ...checkInfo, checked: !checkInfo.checked };
+        return checkInfo;
+      }),
+    );
+  };
+
+  useEffect(() => {
+    const isAnswerSelected = checksInfo.filter(({ checked }) => checked).length > 0;
+    checkAnswerSelected(isAnswerSelected);
+  }, [checksInfo, checkAnswerSelected]);
+
+  return checksInfo.map(({ content, checked }) => (
+    <CheckBox key={content} value={content} checked={checked} onChange={toggleCheck.bind(null, content)} />
+  ));
+};
+
+export default CheckList;
