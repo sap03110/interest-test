c39b89af7e0ccb3a2c975ce6a33d7f8aef0d531f
김미정 <mijeonkim@gmarket.com>
2022-10-12 10:57:24 +0900

    feature: 각 컴포넌트에서 사용되는 상수 분리

---
+++ src/constants/index.js
@@ -0,0 +1,25 @@
+// message text
+export const NO_SELECTION_ALERT_MESSAGE = '목록 중 하나 이상을 선택해 주세요.';
+
+// button text
+export const RADIO_BUTTON_TEXTS = ['매우 낮음 (1점)', '낮음 (2점)', '보통 (3점)', '높음 (4점)', '매우 높음 (5점)'];
+export const NEXT_BUTTON_TEXT = '다음';
+export const RESULT_BUTTON_TEXT = '결과 보기';
+export const ASSIGN_BUTTON_TEXT = '확인';
+export const START_BUTTON_TEXT = '테스트 시작';
+
+// element title
+export const MODAL_TITLE = '관련 역량';
+export const MAIN_SUBTITLE = '각 분야에 대하여, 스스로 생각하는 본인의 역량 정도를 표시하세요.';
+
+// alt text
+export const APP_NAME = '내 성격과 딱 맞는 4차 산업혁명 분야는?';
+export const APP_DESCRIPTION =
+  '개인의 적성, 흥미, 여건 등을 진단하여 이를 통해 맞춤형 교육서비스를 제공하기 위한 테스트';
+
+// links
+export const APP_LINK = 'https://sap03110.github.io/interest-test/';
+export const APP_IMAGE = 'https://k.kakaocdn.net/14/dn/btqZjhpcA2v/MgXIqp1ShQSFl7XWmNEkM1/o.jpg';
+
+// share token
+export const KAKAO_SHARE_TOKEN = '09287439363b5b4c9530bea6c71dd1d4';
